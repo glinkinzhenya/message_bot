51bbd3ea84904f0c2a6872647f8527722c9fba3c
MaksymStepanenko <113137299+MaksymStepanenko@users.noreply.github.com>
2023-11-10 16:21:02 +0200

    add email message/upgate Telegram Bot

--- app.js
+++ app.js
@@ -1,48 +1,11 @@
-
 const TelegramBot = require("node-telegram-bot-api");
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const sendMessage = require("./controler/tuttiKidsControler")
+
 require("dotenv").config();
-const token = process.env.token
 
-
-
-const bot = new TelegramBot(token, { polling: true });
-const chatId = process.env.chatId
-
-// bot.on("message", (msg) => {
-//   lastChatId = msg.chat.id;
-//   // bot.sendMessage(lastChatId, "I'm ready ");
-// });
-
-const sendMessage = (req, res) => {
-  // if (!lastChatId) {
-  //   return res.status(400).json({
-  //     message: "No chat ID available",
-  //   });
-  // }
-
-  const data = req.body;
-  function formatJson(jsonData) {
-    return Object.entries(jsonData)
-      .map(([key, value]) => {
-        if (key === "----------") {
-          return `${key}: ${value}`;
-        }
-        return `${key}: ${value ? value : "м"}`;
-      })
-      .join("\n");
-  }
-
-  const formattedData = formatJson(data);
-
-  bot.sendMessage(chatId, formattedData);
-
-  res.status(200).json({
-    message: "OK",
-  });
-};
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -51,7 +14,7 @@ app.use(logger("dev"));
 app.use(cors());
 app.use(express.json());
 
-app.post("/send", sendMessage);
+app.post("/tutti-kids", sendMessage);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Server is running" });
---
+++ decorators/emailSender.js
@@ -0,0 +1,24 @@
+const nodemailer = require("nodemailer")
+
+require("dotenv").config();
+const { UKR_NET_EMAIL_FROM, UKR_NET_EMAIL_PASSWORD } = process.env;
+
+const nodemailerConfig = {
+  host: "smtp.ukr.net",
+  port: 465,
+  secure: true,
+  auth: {
+    user: UKR_NET_EMAIL_FROM,
+    pass: UKR_NET_EMAIL_PASSWORD,
+  },
+};
+
+const transport = nodemailer.createTransport(nodemailerConfig);
+
+const sendEmail = (data) => {
+  console.log("send");
+  const email = { ...data, from: UKR_NET_EMAIL_FROM };
+  return transport.sendMail(email);
+};
+
+module.exports = sendEmail;
